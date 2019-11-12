fb3a14f13e1c0cb71eaa44bc8cc6fc8df77d52a9
Nag33k <keegan.siebken@gmail.com>
2019-11-12 12:59:46 +0000

    data cleaning script complete

--- 1_decathlon/data_cleaning_scripts/data_cleaning.R
+++ 1_decathlon/data_cleaning_scripts/data_cleaning.R
@@ -1,10 +1,58 @@
 library(tidyverse)
+library(janitor)
 
-#read in data
+#read in data and examine
 
 raw <- read_rds("raw_data/decathlon.rds")
 
-View(raw)
+view(raw)
+
+str(raw)
 
 dim(raw)
 
+names(raw)
+
+top_n(raw, 10)
+
+
+
+
+
+
+
+
+
+
+#clean data with pipe:
+decathlon_tidy <- raw %>%
+  # -pull out rownames
+  rownames_to_column() %>%
+  # -select only relevant columns
+  select("rowname", 
+         "Long.jump", 
+         "100m", 
+         "Points", 
+         "Shot.put", 
+         "Rank",
+         "Competition", 
+         "400m") %>%
+  # -convert names to snake case, both for easier use as columns and to
+  #   more easily identify doubles
+  mutate("rowname" = str_to_lower(rowname)) %>%
+  # -clean column names
+  clean_names() %>%
+  rename("name" = "rowname") %>%
+  # -flip scores to long form
+  pivot_longer(cols = c("long_jump", 
+                        "x100m", 
+                        "shot_put", 
+                        "x400m",
+                        "points"),
+               names_to = "event",
+               values_to = "scores")
+  
+
+view(decathlon_tidy)
+
+write_csv(decathlon_tidy, "clean_data/decathlon_tidy.csv")
