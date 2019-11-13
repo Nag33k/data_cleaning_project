fdb7d43c923b2167ef7824fa89c14d6b0dd0bd0b
Nag33k <keegan.siebken@gmail.com>
2019-11-13 11:27:53 +0000

    data cleaning complete on 3_sea_birds

---
+++ 3_sea_birds/data_cleaning_scripts/data_cleaning_script.R
@@ -0,0 +1,31 @@
+#first things first: libraries, import data
+library(tidyverse)
+library(readxl)
+library(janitor)
+
+ships_raw <- read_excel("raw_data/seabirds.xls", sheet = 1)
+birds_raw <- read_excel("raw_data/seabirds.xls", sheet = 2)
+
+#clean ship data: clean names, select only record ID and latitude 
+ships_raw <- clean_names(ships_raw)
+
+ships_clean <- ships_raw %>%
+  select(record_id, lat)
+
+#clean bird data: clean names, select only bird names and counts
+birds_raw <- clean_names(birds_raw)
+
+birds_clean <- birds_raw %>%
+  rename("common_name" = 
+           "species_common_name_taxon_age_sex_plumage_phase",
+         "scientific_name" = 
+           "species_scientific_name_taxon_age_sex_plumage_phase",
+         "abbreviation" = "species_abbreviation") %>%
+  select(record_id, common_name, scientific_name, abbreviation, count)
+
+#join tibbles and export
+
+obs_clean <- ships_clean %>%
+  inner_join(birds_clean, by = "record_id")
+
+write_csv(obs_clean, "clean_data/obs_clean.csv")
